1427d7c3ef110c885daef26948d751fc2d1b67d3
aonghas43 <mrmoose@gmail.com>
2024-07-14 21:25:28 +0100

    more cleanup

--- data/water.js
+++ data/water.js
@@ -1,11 +1,36 @@
+/* base */
+var histon_boundary = {};
+
+var constituency_boundaries = {};
+
+var internal_drainage_boards = {};
+
+
+/* histon */
+var ad164 = {};
+
+var ad165= {}; 
+
+var histon_waterways = {};
+
+var histon_waterbodies = {};
+
+/* upstream */
+
+var franklin_darwin = {};
+
+var ad164_upstream = {};
+
+
+
+/* downstream */
+
 var beckbrook = {};
 
 var greatouse = {};
 
 var rivercam = {};
 
-var ad164 = {};
-
-var ad165= {}; 
-
 var publicdrain13 = {};
+
+
